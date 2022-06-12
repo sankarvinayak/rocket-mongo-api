37fc02db79ff796db6ab49d9ca255a84c0b82fa7
Sankar vinayak <e.p.sankarvinayak@gmail.com>
2022-06-12 17:40:50 +0530

    Username check success

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+mod api;
+mod models;
+mod repository;
+
+#[macro_use]
+extern crate rocket;
+
+use api::user_api::{create_user, get_user, update_user, delete_user, get_all_users,login}; //import the handler here
+use repository::mongodb_repo::MongoRepo;
+
+#[launch]
+fn rocket() -> _ {
+    let db = MongoRepo::init();
+    rocket::build()
+        .manage(db)
+        .mount("/", routes![create_user])
+        .mount("/", routes![get_user])
+        .mount("/", routes![update_user])
+        .mount("/", routes![delete_user])
+        .mount("/", routes![get_all_users])
+        .mount("/", routes![login])
+}
